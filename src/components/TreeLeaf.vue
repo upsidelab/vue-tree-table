d7a23c4a015506207bac3fd9b070422fb8f931a5
Katarzyna Kurzynska-Madej <kat@upsidelab.io>
2019-07-26 12:01:35 +0200

    refactor: extract default leaf from leaf component

--- src/components/TreeLeaf.vue
+++ src/components/TreeLeaf.vue
@@ -1,22 +1,12 @@
 <template>
-  <div class="row">
-    <div
-      class="indentation"
-      :style="{ width: leftPadding + 'px' }"
-    />
-    <div
-      v-for="key in defaultOrder"
-      :key="key"
-      class="cell"
-    >
-      {{ rowData[key] }}
-    </div>
-  </div>
+  <TreeDefaultLeaf :row-data="rowData" :default-order="defaultOrder" :depth="depth" />
 </template>
 
 <script>
+  import TreeDefaultLeaf from './TreeDefaultLeaf'
   export default {
     name: 'TreeLeaf',
+    components: {TreeDefaultLeaf},
     props: {
       rowData: {
         type: Object,
@@ -30,39 +20,7 @@
         type: Number,
         default: 0
       }
-    },
-    computed: {
-      leftPadding: function () {
-        return this.depth*15
-      }
     }
   }
 </script>
 
-<style scoped>
-  .row{
-    display: flex;
-    flex-flow: row wrap;
-    justify-content: center;
-    padding-left: 15px;
-  }
-
-  .cell{
-    text-align: left;
-    flex-grow: 1;
-    flex-basis: 0;
-    border: solid 0.5px silver;
-    border-left: none;
-    border-right: none;
-    box-sizing: border-box;
-    margin-top: -1px;
-    margin-left: -1px;
-  }
-
-  .indentation{
-    border: solid 0.5px silver;
-    border-left: none;
-    border-right: none;
-    margin-top: -1px;
-  }
-</style>
