f16a369267a7ac5d36d6d63c1b7fcd9b83fc8aa8
Katarzyna Kurzynska-Madej <kat@upsidelab.io>
2019-07-30 10:48:25 +0200

    add missing tests

--- tests/unit/components/TreeBody.spec.js
+++ tests/unit/components/TreeBody.spec.js
@@ -1,5 +1,5 @@
 import TreeBody from '../../../src/components/TreeBody'
-import { shallowMount } from '@vue/test-utils'
+import { shallowMount, mount } from '@vue/test-utils'
 import TreeLeaf from '../../../src/components/TreeLeaf'
 import TreeNode from '../../../src/components/TreeNode'
 
@@ -54,4 +54,51 @@ describe('TreeBody', () => {
     })
   })
 
+  describe('Node template provided', ()=> {
+    const data = [{ name: 'Simba', children: [{name: 'Kiara', children: [{name: 'Koda'}]}] }]
+
+    const wrapper = mount(TreeBody, {
+      propsData: {
+        data,
+        columns: [{id: 'name', label: 'Name'}]
+      },
+      scopedSlots: {
+        nodeTemplate: function(props){
+          return 'Node'
+        }
+      }
+    })
+
+    it('passes slot to node children', () => {
+      const childNodeWrapper = wrapper.findAll(TreeNode).wrappers[0]
+      expect(childNodeWrapper.vm.$scopedSlots).toHaveProperty('nodeTemplate')
+    })
+  })
+
+  describe('Leaf template provided', () => {
+    const data = [{ name: 'Simba', children: [{name: 'Kiara', children: [{name: 'Koda'}]}] }, {name: 'Skaza'}]
+
+    const wrapper = mount(TreeBody, {
+      propsData: {
+        data,
+        columns: [{id: 'name', label: 'Name'}]
+      },
+      scopedSlots: {
+        lefTemplate: function(props){
+          return 'Leaf'
+        }
+      }
+    })
+
+    it('passes slot to node children', () => {
+      const childNodeWrapper = wrapper.findAll(TreeNode).wrappers[0]
+      expect(childNodeWrapper.vm.$scopedSlots).toHaveProperty('leafTemplate')
+    })
+
+    it('passes slot to leaf children', () => {
+      const childLeafWrapper = wrapper.findAll(TreeLeaf).wrappers[0]
+      expect(childLeafWrapper.vm.$scopedSlots).toHaveProperty('leafTemplate')
+    })
+
+  })
 })
