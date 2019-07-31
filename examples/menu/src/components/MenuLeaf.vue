9cda5b2c7948b2ae152c13513ffc3cb92ab96dc7
Katarzyna Kurzynska-Madej <kat@upsidelab.io>
2019-07-31 17:41:19 +0200

    add menu example

---
+++ examples/menu/src/components/MenuLeaf.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="row" v-on:click="changeUrl(rowData.href)">
+        <div
+                class="indentation"
+                :style="{ width: leftPadding + 'px' }"
+        />
+        <div class="cell"> {{ rowData.label }}</div>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'MenuLeaf',
+    props: {
+      rowData: {
+        type: Object,
+        default: () => { return {} }
+      },
+      depth: {
+        type: Number,
+        default: 0
+      }
+    },
+    computed: {
+      leftPadding: function () {
+        return this.depth*15
+      }
+    },
+    methods: {
+      changeUrl(url){
+        window.location.href = url;
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .row{
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: center;
+    padding-left: 15px;
+    background-color: none;
+  }
+
+  .cell{
+    text-align: left;
+    flex-grow: 1;
+    flex-basis: 0;
+    border: solid 0.5px silver;
+    border-left: none;
+    border-right: none;
+    box-sizing: border-box;
+    margin-top: -1px;
+    margin-left: -1px;
+  }
+
+  .indentation{
+    border: solid 0.5px silver;
+    border-left: none;
+    border-right: none;
+    margin-top: -1px;
+  }
+</style>
