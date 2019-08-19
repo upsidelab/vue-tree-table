853a17ac0e07c9dc964903cb83bc5c4ffc4ac09d
Katarzyna Kurzynska-Madej <kat@upsidelab.io>
2019-08-19 16:12:13 +0200

    export default leaf, node & header

--- tree-table/src/package.js
+++ tree-table/src/package.js
@@ -1,3 +1,7 @@
-import TreeTable from "./components/TreeTable";
+import TreeTable from "./components/TreeTable"
+import TreeDefaultLeaf from "./components/TreeDefaultLeaf"
+import TreeDefaultNode from "./components/TreeDefaultNode"
+import TreeTableHeader from "./components/TreeTableHeader"
 
 export default TreeTable
+export {TreeDefaultLeaf, TreeDefaultNode, TreeTableHeader}
